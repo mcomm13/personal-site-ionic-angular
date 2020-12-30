90577d1939afd1efdb59e9d43d93313f75ea5313
Mike Commendatore <michael.commendatore@outlook.com>
2020-12-30 15:57:37 -0500

    Home page complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ContactInfo {
+  facebook: string;
+  twitter: string;
+  linkedin: string;
+  email: string;
+  github: string;
+  youtube: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  contactInfo: ContactInfo = {
+    facebook: 'https://www.facebook.com/profile.php?id=9031270',
+    twitter: 'https://twitter.com/mcomm13',
+    linkedin: 'https://www.linkedin.com/in/michael-commendatore-16654b93/',
+    email: 'michael.commendatore@outlook.com',
+    github: 'https://github.com/mcomm13',
+    youtube: 'https://www.youtube.com/channel/UClY5uTEhLTliK98lIJAD-nw',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
