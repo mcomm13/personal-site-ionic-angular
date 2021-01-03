5a01257fa22d968746464f73f013fd815213e296
Mike Commendatore <michael.commendatore@outlook.com>
2021-01-02 22:03:14 -0500

    Set width of embedded YouTube player to 100%
    Updated URLs to web app links

--- src/app/pages/web-apps/web-apps.page.ts
+++ src/app/pages/web-apps/web-apps.page.ts
@@ -15,13 +15,13 @@ export class WebAppsPage implements OnInit {
       {
         name: 'Leave To Chance',
         description: `When you find yourself having to make a decision between multiple options, and you would prefer to just leave that decision to chance, this app is for you.  Simply enter in the number of possible options that you are choosing between, and this app will randomly pick an option for you.`,
-        url: 'https://leave-to-chance.firebaseapp.com',
+        url: 'https://leave-to-chance.web.app',
       },
       {
         name: 'Football Squares Number Picker',
         description:
           'A web version of my mobile app of the same name.  Use this app if you are running a game of Football Squares (i.e. Super Bowl Squares), and you reach the point where you need to randomly pick the numbers for the headers of your grid.',
-        url: 'https://football-squares-number-picker.firebaseapp.com',
+        url: 'https://football-squares-number-picker.web.app',
       },
     ];
   }
